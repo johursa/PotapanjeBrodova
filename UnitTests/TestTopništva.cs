62929709c2f8a9922c6e7253f0b449155cebdcfd
Josip Hursa <johursa@vsite.hr>
2016-04-27 21:21:08 +0200

    TestoviGotovi

--- UnitTests/TestTopništva.cs
+++ UnitTests/TestTopništva.cs
@@ -12,5 +12,27 @@ namespace UnitTests
             Topnistvo t = new Topnistvo();
             Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
         }
+        [TestMethod]
+        public void Topništvo_PočetnaTaktikaGađanjaNakonPrvogPogotkaJeOkruživanje()
+        {
+            Topnistvo t = new Topnistvo();
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            Assert.AreEqual(TaktikaGađanja.Okruživanje, t.TrenutnaTaktika);
+        }
+        [TestMethod]
+        public void Topništvo_PočetnaTaktikaGađanjaNakonDrugogPogotkaJeSustavnoUništavanje()
+        {
+            Topnistvo t = new Topnistvo();
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            Assert.AreEqual(TaktikaGađanja.SustavnoUništavanje, t.TrenutnaTaktika);
+        }
+        [TestMethod]
+        public void Topništvo_PočetnaTaktikaGađanjaNakonPotonućaJeNapipavanje()
+        {
+            Topnistvo t = new Topnistvo();
+            t.ObradiGađanje(RezultatGađanja.Potonuće);
+            Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
+        }
     }
 }
