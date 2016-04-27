0de1b30e9f607224b51ed083d4c161f664417763
Josip Hursa <johursa@vsite.hr>
2016-04-27 21:13:03 +0200

    TestProšao

---
+++ PotapanjeBrodova/Topnistvo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public enum TaktikaGađanja
+    {
+       Napipavanje,
+       Okruživanje,
+       SustavnoUništavanje
+
+    }
+  public  class Topnistvo
+    {
+        public Topnistvo()
+        {
+            PromjeniTaktikuUNapipavanje();
+        }
+        
+        public Polje UputiPucanj()
+        {
+            throw new NotImplementedException();
+
+
+        }
+        public void ObradiGađanje(RezultatGađanja rezultat)
+        {
+
+        }
+        private void PromjeniTaktikuUNapipavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Napipavanje;
+
+        }
+        private void PromjeniTaktikuUOkruživanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Okruživanje;
+        }
+        private void PromjeniTaktikuUSustavnoUništavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.SustavnoUništavanje;
+
+        }
+
+        public TaktikaGađanja TrenutnaTaktika
+        {
+            get; private set;
+
+        }
+    }
+}
---
+++ UnitTests/TestTopništva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+namespace UnitTests
+{
+    [TestClass]
+    public class TestTopništva
+    {
+        [TestMethod]
+        public void Topništvo_PočetnaTaktikaGađanjaJeNapipavanje()
+        {
+            Topnistvo t = new Topnistvo();
+            Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
+        }
+    }
+}
